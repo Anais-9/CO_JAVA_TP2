d07b2e5fccaf145901c59f33364489f038e9786c
anais <anais@Ana>
2025-10-22 11:04:27 +0200

    Sauvegarde

--- TP2/src/Pokemon/Pokemon0.java
+++ TP2/src/Pokemon/Pokemon0.java
@@ -1,5 +1,33 @@
 package Pokemon;
+import java.util.Random;
 
 public class Pokemon0 {
-
+	String nom;
+	int niveau;
+	int hp;
+	float atk;
+	
+	public Pokemon0(String nom) {
+		this.nom = nom;
+		Random r = new Random();
+		this.niveau = r.nextInt(1, 10);
+		this.hp = this.niveau*2;
+		this.atk = (this.niveau / 2 )+1;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getNiveau() {
+		return this.niveau;
+	}
+	
+	public int getHp() {
+		return this.hp;
+	}
+	
+	public float getAtk() {
+		return this.atk;
+	}
 }
